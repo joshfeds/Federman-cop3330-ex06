02dab1d575d6ac44d785c0fce7dd960313fb5129
joshfeds14 <joshfeds14@gmail,com>
2021-06-04 18:06:40 -0400

    Exercise 06

---
+++ src/main/java/oop/Exercise06/Exercise06.java
@@ -0,0 +1,12 @@
+package oop.Exercise06;
+import java.util.Scanner;
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Joshua Federman
+ */
+public class Exercise06
+{
+    public static void main( String[] args ) {
+
+    }
+}
