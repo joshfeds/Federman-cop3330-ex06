157bc82df58c470bc54d24d15944a3c070c66b84
joshfeds14 <joshfeds14@gmail,com>
2021-06-04 21:17:05 -0400

    Exercise 06

--- src/main/java/oop/Exercise06/Exercise06.java
+++ src/main/java/oop/Exercise06/Exercise06.java
@@ -1,12 +1,36 @@
 package oop.Exercise06;
+import java.time.LocalDate;
 import java.util.Scanner;
+
+
 /*
  *  UCF COP3330 Summer 2021 Assignment 1 Solution
  *  Copyright 2021 Joshua Federman
  */
 public class Exercise06
 {
+    static Scanner input = new Scanner(System.in);
+    static LocalDate year = LocalDate.now();
+    public static String currAge() {
+        System.out.print("What is your current age? ");
+        return input.nextLine();
+    }
+    public static String ageRetire() {
+        System.out.print("At what age would you like to retire? ");
+        return input.nextLine();
+    }
+    public static void yearsLeft(int age, int retireAge) {
+        int yearsLeft = retireAge - age;
+        System.out.printf("You have %d years left until you can retire.\n", yearsLeft);
+        int currYear = year.getYear();
+        int retYear = currYear + yearsLeft;
+        System.out.printf("It's %d, so you can retire in %d", currYear, retYear);
+    }
     public static void main( String[] args ) {
+
+        int age = Integer.parseInt(currAge());
+        int retireAge = Integer.parseInt(ageRetire());
+        yearsLeft(age, retireAge);
 
     }
 }
